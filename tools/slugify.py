f2f328ba4bd420e5ffcbe53af54cc8aedd8cab41
wilbur ma <wilbur.ma@hotmail.com>
2013-09-11 14:58:26 +0800

    fix slugify tool bug

--- tools/slugify.py
+++ tools/slugify.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
+# @author: wilbur.ma@foxmail.com
+# @date: 2013-09-09
+# @license: BSD 3-Clause License
+# @brief: the default slugify function used by extract_headings plugin
+# @usage: grep '^#' content/pages/about.md | sed 's/#//g' | xargs -d '\n' ./slugify.py
 
-import sys, md5
+import sys, md5, optparse
 
 def slugify(inputStr):
     md5obj = md5.new()
     md5obj.update(inputStr)
-    return "{} => #{}".format(inputStr, md5obj.digest().encode("hex"))
+    return "#{} <= {}".format(md5obj.digest().encode("hex"), inputStr)
 
 if __name__ == "__main__":
-    print(slugify(sys.stdin.read().strip()))
+    inputStr = sys.stdin.read()
+    if not inputStr:
+        parser = optparse.OptionParser()
+        options, args = parser.parse_args()
+        for inputStr in args:
+            print(slugify(inputStr.strip()))
+    else:
+        print(slugify(sys.stdin.read().strip()))
 
