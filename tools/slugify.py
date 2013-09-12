c32ab9c8200457960bc3619dd92e402902bd3ec7
wilbur ma <wilbur.ma@hotmail.com>
2013-09-12 09:38:13 +0800

    fix slugify tool

--- tools/slugify.py
+++ tools/slugify.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
         for inputStr in args:
             print(slugify(inputStr.strip()))
     else:
-        print(slugify(inputStr.strip))
+        print(slugify(inputStr.strip()))
 
