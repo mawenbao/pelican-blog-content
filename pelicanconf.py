b9cd198cd847c352a6cb6b5f22311a3b331f570f
wilbur ma <mawenbao@hotmail.com>
2014-02-11 14:42:13 +0800

    update config: footer

--- pelicanconf.py
+++ pelicanconf.py
@@ -170,8 +170,8 @@ NIUX2_HEADER_DROPDOWN_SECTIONS = {
             ],
         }
 NIUX2_FOOTER_LINKS = [
-        ('About', 'about me', '/about.html', ''),
-        ('Agreement', 'terms, license and privacy etc.', '/agreement.html', ''),
+        ('关于', 'about me', '/about.html', ''),
+        ('协议', 'terms, license and privacy etc.', '/agreement.html', ''),
         ]
 
 NIUX2_FOOTER_ICONS = [
