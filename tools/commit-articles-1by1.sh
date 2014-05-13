366260019ff6015216e6e45fd878d96ec7033bb8
wilbur ma <mawenbao@hotmail.com>
2014-05-13 09:56:02 +0800

    update tool script

--- tools/commit-articles-1by1.sh
+++ tools/commit-articles-1by1.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 # @author: mawenbao@hotmail.com
 # @date: 2014-05-12
-# @desc: find all the modified files in content/ directory
-#        and commit them one at a tim
+# @desc: find all the untracked/modified files
+#        in content/ directory and commit them one at a time
 
 COMMIT_MSG="update article"
-MODIFIED_FILES=`git reset >/dev/null && git ls-files -m | grep '^content' | grep -E '(md|rst)$'`
 
-for f in ${MODIFIED_FILES}; do
+function filter_articles() {
+    echo `echo $1 | grep '^content' | grep -E '(md|rst)$'`
+}
+
+git reset > /dev/null
+
+MODIFIED_FILES=`filter_articles $(git ls-files -m)`
+UNTRACKED_FILES=`filter_articles $(git ls-files -o --exclude-standard)`
+
+for f in ${MODIFIED_FILES} ${UNTRACKED_FILES}; do
     git add ${f}
     git commit -m "${COMMIT_MSG}"
 done
